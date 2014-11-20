96412f195f21dce4abecef8a4ea9a2c2ba49ea3d
Alex Lutz <alutz@netdocuments.com>
2014-11-19 20:15:39 -0700

    fixed so it *should* work...

---
+++ ghost/tools/chocolateyUninstall.ps1
@@ -0,0 +1,24 @@
+try {
+  $dest = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+  $packageName = "ghost"
+  $packageVersion = "0.5.5"
+  $serviceName = "ghost.exe"
+  $zipFileName = "ghost-0.5.5.zip"
+  $ghostInstalledDir = ($env:ChocolateyInstall + "\lib\" + $packageName + "." + $packageVersion)
+
+  Write-Host "Stopping the service `'$serviceName`'."
+  sc stop $serviceName
+
+  cd $postUnzipDir
+  Write-Host "Removing the service `'$serviceName`'."
+  node .\DeleteService.js
+
+  # UnInstall Zip
+  UnInstall-ChocolateyZipPackage "$packageName" "$zipFileName"
+
+  Write-ChocolateySuccess $packageName
+} catch {
+  Write-ChocolateyFailure $packageName $($_.Exception.Message)
+  throw
+}
