ea5cf481e98df819ab5d6dbd5196ea0aa8450d60
Alex Lutz <alutz@netdocuments.com>
2014-11-19 18:03:03 -0700

    Removed comments.

--- ghost/tools/uninstallghost.ps1
+++ ghost/tools/uninstallghost.ps1
@@ -3,24 +3,14 @@ try {
   $serviceName = "ghost.exe"
   $binRoot = Get-BinRoot
   $ghostDir = Join-Path $binRoot $packageName
-
-  # If Ghost is installed, uninstall it. Else, give a message.
   if ($ghostDir) {
-    # Enter the ghost directory for uninstallation
     cd $ghostDir
-
-    # Stop the ghost windows service
     Write-Host "Stopping the service `'$serviceName`'."
     & sc stop $serviceName
-
-    # Remove the ghost windows service
     Write-Host "Removing the service `'$serviceName`'."
     node .\DeleteService.js
-
-    # Remove the installation files
     Write-Host "Removing files at `'$ghostDir`'."
     Remove-Item $ghostDir -Recurse -Force
-
     Write-ChocolateySuccess $packageName
   } else {
       Write-Host "$packageName is not installed on the system. Nothing to do."
