7e0d3be5ceb43a63beae68e65bd00cf5c3640fb9
Alex Lutz <alexinslc@gmail.com>
2014-11-22 13:29:44 -0700

    Fixed syntax.

--- rvtools/tools/chocolateyInstall.ps1
+++ rvtools/tools/chocolateyInstall.ps1
@@ -1,10 +1,5 @@
 $packageName = 'rvtools'
-$installerType = 'msi' #only one of these: exe, msi, msu
-$url = 'http://robware.net/download/501D8EE9-B871-45FB-B2A1-9BE0B7BFF025/RVTools.msi' # download url
+$installerType = 'msi'
+$url = 'http://robware.net/download/501D8EE9-B871-45FB-B2A1-9BE0B7BFF025/RVTools.msi'
 $silentArgs = '/quiet' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
-Write-ChocolateySuccess "$packageName"
-} catch {
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw
-}
