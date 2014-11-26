299554b7eb2aab5b1976d40daaea3f8f33caa43c
Alex Lutz <alutz@netdocuments.com>
2014-11-26 09:24:11 -0700

    Updated version numbers, they don't need to change unless a BIG thing is changing.

--- rvtools/tools/chocolateyInstall.ps1
+++ rvtools/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName = 'rvtools'
 $installerType = 'msi'
 $url = 'http://robware.net/download/501D8EE9-B871-45FB-B2A1-9BE0B7BFF025/RVTools.msi'
-$silentArgs = '/quiet' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
+$silentArgs = '/quiet'
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
--- wireedit/tools/chocolateyInstall.ps1
+++ wireedit/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName = 'wireedit'
 $installerType = 'msi'
 $url = 'https://wireedit.com/WireEditiWin7-0.11.430.msi'
-$silentArgs = '/quiet' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
+$silentArgs = '/quiet'
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
