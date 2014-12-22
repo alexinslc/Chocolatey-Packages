601ff705b81f7833af8a59352969ee5760bb671f
alexinslc <alexinslc@gmail.com>
2014-12-21 17:44:10 -0700

    Updated wireedit.

--- wireedit/tools/chocolateyInstall.ps1
+++ wireedit/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName = 'wireedit'
 $installerType = 'msi'
-$url = 'https://wireedit.com/WireEditiWin7-0.11.430.msi'
+$url = 'https://wireedit.com/0.11.462/WireEditWin7-0.11.462.msi'
 $silentArgs = '/quiet'
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
